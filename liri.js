b70cec5f5d928bc48af92cb6396820eef41a5b92
Christopher Ruiz <chrisruizaz@gmail.com>
2019-10-16 15:30:00 -0700

    I added some more functions. The random txt finally works. Working on the OMDB to make it work

--- liri.js
+++ liri.js
@@ -2,16 +2,39 @@
 const args = process.argv;
 const command = args[2];
 var search = process.argv.slice(3).join(" ");
+const fs = require("fs");
 liriCommand(search);
 function liriCommand(search) {
   if (command === "spotify-this-song") {
     spotifyThis(search);
-  };
- else if (command === "concert-this"){};
+  } else if (command === "concert-this") {
+    bandsinTown(search);
+  } else if (command === "movie-this") {
+    movieIMDB(search);
+  } else if (command === "do-what-it-says") {
+    fs.readFile(
+      "random.txt",
+      "utf-8",
+      //"utf-8" will make actual things populate otherwise it will be all numbers!
+      (err, data) => {
+        console.log(data);
+      }
+    );
+  }
 }
 
 function spotifyThis(search) {
-  console.log(search);
+  var keys = require("./keys.js");
+  var spotify = new Spotify(keys.spotify);
+  console.log(spotify);
+}
+
+function bandsinTown(search) {
+  console.log("bands in town");
+}
+
+function movieIMDB(search) {
+  console.log("movie search");
 }
 
 //need to create a function that uses the input to find
