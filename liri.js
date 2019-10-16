969ff1d616e168333e324ccefe48cf6bccf8c8d2
Christopher Ruiz <chrisruizaz@gmail.com>
2019-10-15 21:51:54 -0700

    added some functions

--- liri.js
+++ liri.js
@@ -1,10 +1,19 @@
-require("dotenv").config();
+// require("dotenv").config();
 const args = process.argv;
 const command = args[2];
-
-if (command === "spotify-this-song") {
-  SpotSong(input);
+var search = process.argv.slice(3).join(" ");
+liriCommand(search);
+function liriCommand(search) {
+  if (command === "spotify-this-song") {
+    spotifyThis(search);
+  };
+ else if (command === "concert-this"){};
 }
+
+function spotifyThis(search) {
+  console.log(search);
+}
+
 //need to create a function that uses the input to find
 //     * Artist(s)
 
