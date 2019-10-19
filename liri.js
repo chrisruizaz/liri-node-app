b772d57afbd7d8a22b81eb2321540d7bdba52d5e
Christopher Ruiz <chrisruizaz@gmail.com>
2019-10-18 22:23:29 -0700

    made the function for IMDB work. made a function for the concert this but I have no idea what this information from the response is

--- liri.js
+++ liri.js
@@ -12,14 +12,9 @@ function liriCommand(search) {
   } else if (command === "movie-this") {
     movieIMDB(search);
   } else if (command === "do-what-it-says") {
-    fs.readFile(
-      "random.txt",
-      "utf-8",
-      //"utf-8" will make actual things populate otherwise it will be all numbers!
-      (err, data) => {
-        console.log(data);
-      }
-    );
+    fs.readFile("random.txt", "utf-8", (err, data) => {
+      console.log(data);
+    });
   }
 }
 
@@ -30,11 +25,58 @@ function spotifyThis(search) {
 }
 
 function bandsinTown(search) {
-  console.log("bands in town");
+  var axios = require("axios");
+
+  var queryUrl =
+    "https://rest.bandsintown.com/artists/" +
+    search +
+    "/events?app_id=codingbootcamp";
+  axios.get(queryUrl).then(function(response) {
+    console.log(response);
+  });
 }
 
 function movieIMDB(search) {
-  console.log("movie search");
+  var axios = require("axios");
+
+  var queryUrl =
+    "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
+
+  axios
+    .get(queryUrl)
+    .then(function(response) {
+      // * Title of the movie.
+      console.log("Title: " + response.data.Title);
+      // * Year the movie came out.
+      console.log("Release Year: " + response.data.Year);
+      // * IMDB Rating of the movie.
+      console.log("IMDB Rating: " + response.data.imdbRating);
+      // * Rotten Tomatoes Rating of the movie.
+      console.log("Metascore: " + response.data.Metascore);
+      // * Country where the movie was produced.
+      console.log("Country: " + response.data.Country);
+      // * Language of the movie.
+      console.log("Language: " + response.data.Language);
+      // * Plot of the movie.
+      console.log("Plot: " + response.data.Plot);
+      // * Actors in the movie.
+      console.log("Actors: " + response.data.Actors);
+    })
+    .catch(function(error) {
+      if (error.response) {
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
 }
 
 //need to create a function that uses the input to find
